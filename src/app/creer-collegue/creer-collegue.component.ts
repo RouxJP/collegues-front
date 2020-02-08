ba18817bba45a464c35ebb1c1eac19dfa0800ccf
Roux JP <jproux343@gmail.com>
2020-02-08 14:30:42 +0100

    NgForm

--- src/app/creer-collegue/creer-collegue.component.ts
+++ src/app/creer-collegue/creer-collegue.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Collegue } from '../models/Collegue';
 import { DataService } from '../services/data.service';
 import {FormControl } from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-creer-collegue',
@@ -20,7 +21,7 @@ export class CreerCollegueComponent implements OnInit {
     this.creaCollegue.photoUrl = 'https://www.w3schools.com/bootstrap/img_avatar2.png';
   }
 
-  creerCollegue(etatForm: FormControl) {
+  creerCollegue(etatForm: NgForm) {
     this.messageErreur = null;
     this.messageOk = null;
 
